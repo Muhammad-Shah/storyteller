c3ce8be102d71a8f5845ccc8e57ce825f62c7898
Muhammad-Shah <msh87376@gmail.com>
2024-07-09 12:55:23 +0500

    ne

--- next.config.js
+++ next.config.js
@@ -5,26 +5,27 @@ const nextConfig = {
           source: "/api/:path*",
           destination:
             process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/api/:path*"
-              : "/api/:path*",
+              ? "http://127.0.0.1:8000/api/:path*"  // Local development API endpoint
+              : "https://storyteller-coral.vercel.app/api/:path*",  // Production API endpoint on Vercel
         },
         {
           source: "/docs",
           destination:
             process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/docs"
-              : "/api/docs",
+              ? "http://127.0.0.1:8000/docs"  // Local development API docs endpoint
+              : "https://storyteller-coral.vercel.app/api/docs",  // Production API docs endpoint on Vercel
         },
         {
           source: "/openapi.json",
           destination:
             process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/openapi.json"
-              : "/api/openapi.json",
+              ? "http://127.0.0.1:8000/openapi.json"  // Local development OpenAPI endpoint
+              : "https://storyteller-coral.vercel.app/api/openapi.json",  // Production OpenAPI endpoint on Vercel
         },
       ];
     },
   };
   
   module.exports = nextConfig;
+  
   
