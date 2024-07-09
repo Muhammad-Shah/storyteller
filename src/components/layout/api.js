7902f425d96ca50b023db49a711c86a1e7016b87
Muhammad-Shah <msh87376@gmail.com>
2024-07-09 12:02:59 +0500

    api

--- src/components/layout/api.js
+++ src/components/layout/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://storyteller-coral.vercel.app/api || http://127.0.0.1:8000/",
+  baseURL: "https://storyteller-coral.vercel.app/api",
 });
 
 export default api;
