eb33ea329025d98a65c37eff5707e52d5e352176
Muhammad-Shah <msh87376@gmail.com>
2024-07-09 18:18:47 +0500

    main

--- api/main.py
+++ api/main.py
@@ -51,7 +51,7 @@ class CompleteRequest(BaseModel):
     setting_description: Optional[str] = None
 
 
-@app.post("/generate")
+@app.post("/api/generate")
 def generate_story(request: GenerateRequest):
     response = generate(
         topic=request.topic,
