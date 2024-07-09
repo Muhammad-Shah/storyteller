a005e4b4f80ce2ff93cb3c08c5bdbf24977b3827
Muhammad-Shah <msh87376@gmail.com>
2024-07-09 21:41:29 +0500

    js

--- next.config.js
+++ next.config.js
@@ -1,36 +1,29 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = {
-  async headers() {
+const nextConfig = {
+  rewrites: async () => {
     return [
       {
         source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value:
-              "https://storyteller-lknagocza-muhammad-shahs-projects.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
       },
-    ];
-  },
-  async rewrites() {
-    return [
       {
-        source: "/api/:path*",
-        destination: "https://storyteller-coral.vercel.app/api/:path*",
+        source: "/docs",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/docs"
+            : "/api/docs",
+      },
+      {
+        source: "/openapi.json",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/openapi.json"
+            : "/api/openapi.json",
       },
     ];
   },
 };
+
+module.exports = nextConfig;
