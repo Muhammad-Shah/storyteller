320541637d5f7fed36bb14f4227a44058a3035c2
Muhammad-Shah <msh87376@gmail.com>
2024-07-09 15:46:27 +0500

    api

--- src/components/layout/api.js
+++ src/components/layout/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:8000" || "https://storyteller-coral.vercel.app/api",
+    "http://localhost:8000" ||
+    "https://storyteller-liart.vercel.app/api" ||
+    "https://storyteller-git-master-muhammad-shahs-projects.vercel.app/api" ||
+    "https://storyteller-60m87xh7q-muhammad-shahs-projects.vercel.app/api",
 });
 
 export default api;
